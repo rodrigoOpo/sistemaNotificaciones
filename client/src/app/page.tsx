a7da1ad43e98eebd369fdaa404d492f412aa7a61
Rodrigo <rodrioppo2006@gmail.com>
2026-05-13 20:14:24 +0200

    set up basic notification

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,10 +1,12 @@
 import Form from "@/ui/form";
+import Notification from "@/ui/Notification";
 
 export default function Home() {
   return (
     <>
       <h1>home</h1>
       <Form/>
+      <Notification/>
     </>
   );
 }
